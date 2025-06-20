b749bd2661d57f0b80c1702d82bf0bd8fffdce8d
Sweta7 <anvita1982@gmail.com>
2025-06-20 15:10:34 +1000

    React app setup for breathing app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import BreatheCircle from "./BreatheCircle";
+
+function App() {
+ return (
+    <div>
+      <h1>Hello, Vite + React!</h1>
+      <p>This is your main application component.</p>
+      {/* You can add other components here */}
+    </div>
+  )
+}
+
+export default App;
